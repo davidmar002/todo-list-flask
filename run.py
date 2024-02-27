187f9167b590acef83384fda85997b760665209f
David Martinez <davidmtzatom@gmail.com>
2024-02-27 15:59:44 -0600

    todo-list-v1

---
+++ run.py
@@ -0,0 +1,13 @@
+from todor import create_app                   # Con este archivo podemos ejecutar la aplicacion
+
+app = create_app()
+
+
+if __name__ =='__main__':
+    app.run()
+
+
+
+
+
+# David Martinez Diaz
